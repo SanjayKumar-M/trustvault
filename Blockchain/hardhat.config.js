b14821c6e75810557d8dc1ae8543b2c2b064568e
Sanjay Kumar M <sanjaymoad786@gmail.com>
2023-02-19 13:50:40 +0530

    Update hardhat.config.js

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.17",
   networks:{
     goerli:{
-      url:'https://eth-goerli.g.alchemy.com/v2/7LGvqMTpRaoj6JIzhVdCYZW-GWmnOihO',
-      accounts:['f015034756ed14b746239a0b5c8b6f97c00f4a51f539aefa6dcc570372fc4c32'],
+      url:'',
+      accounts:[''],
 
     },
   }
