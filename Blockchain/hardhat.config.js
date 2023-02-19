a55b79e0738c8266fde4bbe7d909a80cd1f1e562
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-02-19 23:19:47 +0530

    Security

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -5,14 +5,14 @@ module.exports = {
   networks: {
     mumbai: {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/sDgBhv6AbwLSq3WMmDt3P6dFqPY-Rre0',
-      accounts: ['8736319d495123dada9b10099ddc0722624aaad321aa99df40cf620119c5fcce'],
+      accounts: ['Private-Key'],
 
     },
 
     hyperspace: {
       chainId: 3141,
       url: "https://api.hyperspace.node.glif.io/rpc/v1",
-      accounts: ['8736319d495123dada9b10099ddc0722624aaad321aa99df40cf620119c5fcce'],
+      accounts: ['Private-Key'],
     },
 
   }
